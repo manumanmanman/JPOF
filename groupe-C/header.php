2538df789255c26b1f5231ed8ba155566f2b1cf1
Eric Joppart <samseric@hotmail.com>
2020-10-07 13:53:10 +0200

    creation groupe-c

---
+++ groupe-C/header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Activités
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Détail activités</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Conférenciers
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Fiche conférencier</a>
+              </div>
+            </li>
+            
+            <li class="nav-item">
+              <a class="nav-link" href="#">À porpos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    
+
+
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ groupe-C/index.php
@@ -0,0 +1,17 @@
+<?php include('header.php');?>
+<div>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi omnis amet minus eaque dicta quis perspiciatis, maiores beatae quaerat, neque eius ratione esse animi consequatur, molestias commodi! Unde, tenetur assumenda.</p>
+</div>
+<div>
+  <p>Ipsum consequatur voluptate iste necessitatibus, nesciunt nulla optio, dicta quo velit veritatis sit, officia nihil libero. Quam placeat veniam omnis ducimus, error id reprehenderit nesciunt eum officia. Deserunt, ratione unde.</p>
+</div>
+<div>
+  <p>Ipsum saepe nobis eaque veritatis! Veritatis, debitis quasi fuga commodi in autem nesciunt quos porro suscipit vel delectus quod saepe sequi adipisci exercitationem tenetur earum iure! Corrupti temporibus id ducimus.</p>
+</div>
+<div>
+  <p>Dolorum facilis deserunt quia voluptate voluptatem. Harum accusantium officiis ab excepturi deleniti exercitationem, ea odio. Quas, ipsa sequi tenetur nobis dolores eius natus atque adipisci magnam corporis, iusto, eaque enim?</p>
+</div>
+<div>
+  <p>Sed, ut veniam. Sed, at ad necessitatibus ea quisquam veniam nesciunt, nisi quidem odio atque ratione quas dolorem temporibus totam id doloribus nam ipsum, quam magnam natus. Molestiae, ut pariatur.</p>
+</div>
+<?php include('footer.php');?>
