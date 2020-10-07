b998a596deafab2ee526875491d914a1f7dc5df2
Julien De coen <juliendecoen2308@gmail.com>
2020-10-07 11:50:32 +0200

    jpof-groupe-1

---
+++ groupe-A/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="scss/bootstrap.min.css">
+    <link rel="stylesheet" href="scss/style.css">
+    <title></title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item mx-3">
+        <a class="nav-link" href="#">Accueil</a>
+      </li>
+      <li class="nav-item mx-3">
+        <a class="nav-link" href="#">Conférenciers</a>
+      </li>
+      <li class="nav-item mx-3">
+        <a class="nav-link" href="#">À propos</a>
+      </li>
+      <li class="nav-item ml-3 mr-5">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+    <a class="nav-link ml-5" href="#">Inscription - Connexion</a>
+  </div>
+</nav>
