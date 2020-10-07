ffda9f5bf08540f221eac520e3339c72832432ac
Ayoub Benlamin <ayoubbenlamin@hotmail.com>
2020-10-07 11:31:20 +0200

    init groupe D

---
+++ groupe-D/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>JPOF SARAH</title>
+</head>
+
+<body>
+    
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav mx-auto">
+    <li class="nav-item active">
+        <a class="nav-link" href="#"><img src="img/heff_logo.png" alt=""></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="activites.php">Activités</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="conferenciers.php">Conférenciers</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">A propos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
