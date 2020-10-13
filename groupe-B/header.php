feb369ccd2881691da9afd1de7e9058b00aa6b3f
Eden Maestro <edenkonde@gmail.com>
2020-10-13 14:29:27 +0200

    j'ai cahngé le menu

--- groupe-B/header.php
+++ groupe-B/header.php
@@ -8,12 +8,16 @@
 	<link href="scss/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
     <a class="navbar-brand" href="index.php"><img src="img/heff_logo.png" alt=""></a>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
     <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
       <li class="nav-item active">
         <a class="nav-link" href="index.php">Accueil<span class="sr-only">(current)</span></a>
@@ -28,9 +32,6 @@
         <a class="nav-link" href="conferenciers.php">Profil</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    
   </div>
 </nav>
