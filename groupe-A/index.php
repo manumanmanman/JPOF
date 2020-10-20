fd26a5fbf09b2813409c8439c9240bf40e444092
Julien De coen <juliendecoen2308@gmail.com>
2020-10-20 15:34:49 +0200

    style carte et variables scss

--- groupe-A/index.php
+++ groupe-A/index.php
@@ -8,12 +8,12 @@
 <div class="container">
         <div class="row">
 
-            <div class="col-lg-6 card-entier">
+            <div class="col-lg-6 card-entier pedagogique">
                 
                     <div class="cartes row">
                     <div class="carte card-left col-6">
                         <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
-                        <h2>Techniques graphiques</h2>
+                        <h2>pedagogique</h2>
                     </div>
                     <div class="col-6 carte card-right">
                         <h1>Atelier Ecape Game</h1>
@@ -30,6 +30,31 @@
                     </div>
                 
             </div> <!--  card fin -->
+
+            <div class="col-lg-6 card-entier paramedical">
+              
+                    <div class="cartes row">
+                    <div class="carte card-left col-6">
+                        <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
+                        <h2>Paramedical</h2>
+                    </div>
+                    <div class="col-6 carte card-right">
+                        <h1>Atelier Ecape Game</h1>
+                        <h4>De 11h à 13h</h4>
+                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
+                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
+
+                        <span>Aneessens</span>
+                        <h5>local 140</h5>
+
+                        
+                        <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
+                    </div>
+                    </div>
+                
+                
+            </div> <!-- card fin -->
+
 
             <div class="col-lg-6 card-entier">
               
