994759d07a2e7a0b44ce3652e1fca60bbaf70258
Emmanuel Lemal <man@elastik.eu>
2020-10-20 15:41:54 +0200

    php HP ok + début modal

--- prof/footer.php
+++ prof/footer.php
@@ -1,4 +1,31 @@
-    <script src="js/jquery-3.5.1.min.js"></script>
+   
+   <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <!-- <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div> -->
+      <div class="modal-body">
+      <form action="connexion.php" method="post">
+        <input type="text" name="email" placeholder="votre email">
+        <input type="password" name="mdp" placeholder="mot de passe">
+        <input type="submit" value="login" class="btn btn-success">
+        </form> 
+      </div>
+      <!-- <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div> -->
+    </div>
+  </div>
+</div>
+   
+   
+   <script src="js/jquery-3.5.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/index.js"></script>
 </body>
--- prof/index.php
+++ prof/index.php
@@ -12,10 +12,7 @@ $sql = " SELECT * FROM activities
           LEFT JOIN buildings ON activities.building_id = buildings.building_id
           LEFT JOIN categories ON activities.category_id = categories.category_id
           LEFT JOIN speakers ON activities.activity_speaker = speakers.speaker_id
-
-
 ";
-
 $activities = $conn->query($sql);
 foreach ($activities as $activity) {    // Début de la boucle
 
@@ -28,25 +25,37 @@ foreach ($activities as $activity) {    // Début de la boucle
     $heurefin = strtotime( $activity['activity_end'] );
     $heurefin = strftime("%Hh%M", $heurefin);
 
-
-
     ?>
-
-            <div class="col-lg-6 card-entier">
+            <div id="id_<?php echo $activity["activity_id"]; ?>" class="col-lg-6 card-entier <?php echo $activity["category_slug"]; ?>">
               
                     <div class="cartes row">
                     <div class="carte card-left col-6">
                         <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
-                        <h2>Techniques graphiques</h2>
+                        <h2><?php echo utf8_encode($activity["category_name"]); ?></h2>
                     </div>
                     <div class="col-6 carte card-right">
                         <h1><?php echo utf8_encode($activity["activity_name"]); ?></h1>
                         <h4>De 11h à 13h</h4>
-                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
-                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
 
-                        <span>Aneessens</span>
-                        <h5>local 140</h5>
+                        <!-- si l'utilisateur est logué, on fait une query ajax pour engregistrer cette activité dans ses favoris -->
+                        <?php if($_SESSION["user"] !="") { ?>
+                            <!-- <a class= "coeur" href="query-ajax.php"><img src="img/image-coeur-png-blanc.png" alt="coeur"></a> -->
+                            
+                        <!-- s'il n'est pas logué, on lui propose de se loguer/enregister -->
+                        <?php } else { ?>
+                            <a class= "coeur" href="register.php"  data-toggle="modal" data-target="#exampleModal"><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
+
+                        <?php } ?>
+
+                        
+
+
+
+
+                        <p><?php echo utf8_encode($activity["activity_description"]); ?></p>
+
+                        <span>Campus: <?php echo utf8_encode($activity["building_name"]); ?></span>
+                        <h5>Local: <?php echo utf8_encode($activity["room_name"]); ?></h5>
 
                         
                         <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
