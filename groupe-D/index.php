70797b446d2521be8dced3d97322488e3c32e787
Ayoub Benlamin <ayoubbenlamin@hotmail.com>
2020-10-20 09:46:43 +0200

    ajout dans la page d'accueil les activités

--- groupe-D/index.php
+++ groupe-D/index.php
@@ -1,9 +1,117 @@
 
 <?php include("header.php");?>
 
-<div id="sarah">contenu</div>
-<div id="ayoub">
-    contenu
+
+
+
+</div>
+<nav class="navbar navbar-expand-lg justify-content-center">
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav mx-auto">
+    <li class="nav-item active">
+        <a class="nav-link black" href="#"> Toutes les activités</a>
+      </li>
+    <li class="nav-item active">
+        <a class="nav-link mauve" href="#"> Techniques</a>
+      </li>
+    <li class="nav-item active">
+        <a class="nav-link orange" href="#"> Economique et Social</a>
+      </li>
+    <li class="nav-item active">
+        <a class="nav-link rouge" href="#"> Paramédical</a>
+      </li>
+    <li class="nav-item active">
+        <a class="nav-link vert" href="#"> Arts Appliqués</a>
+      </li>
+    <li class="nav-item active">
+        <a class="nav-link bleu" href="#"> Pédagogique</a>
+      </li>
+      
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
+ <div class="row">
+        <div class="col-12 col-md-4">
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="https://via.placeholder.com/300x180/0000FF/808080 " alt="Card image cap">
+
+    <div class="carre">
+    <i class="far fa-bookmark">   Paramédical</i>
+    <i class="far fa-calendar-alt">    Date et Heure</i>
+    <i class="fas fa-map-marker-alt">  Site du Palais du Midi</i>
+    <i class="fas fa-microphone">   Mr. John Doe</i>
+
+    </div>
+
+  <div class="card-body">
+    <h5 class="card-title">Titre</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="favori"><i class="far fa-heart"></i></a>
+  </div>
+
 </div>
 
+
+    
+</div>
+  
+<div class="col-12 col-md-4">
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="https://via.placeholder.com/300x180/0000FF/808080 " alt="Card image cap">
+
+    <div class="carre">
+    <i class="far fa-bookmark">   Paramédical</i>
+    <i class="far fa-calendar-alt">    Date et Heure</i>
+    <i class="fas fa-map-marker-alt">  Site du Palais du Midi</i>
+    <i class="fas fa-microphone">   Mr. John Doe</i>
+
+    </div>
+
+  <div class="card-body">
+    <h5 class="card-title">Titre</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="favori"><i class="far fa-heart"></i></a>
+  </div>
+
+</div>
+
+
+    
+</div>
+
+<div class="col-12 col-md-4">
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="https://via.placeholder.com/300x180/0000FF/808080 " alt="Card image cap">
+
+    <div class="carre">
+    <i class="far fa-bookmark">   Paramédical</i>
+    <i class="far fa-calendar-alt">    Date et Heure</i>
+    <i class="fas fa-map-marker-alt">  Site du Palais du Midi</i>
+    <i class="fas fa-microphone">   Mr. John Doe</i>
+
+    </div>
+
+  <div class="card-body">
+    <h5 class="card-title">Titre</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="favori"><i class="far fa-heart"></i></a>
+  </div>
+
+</div>
+
+
+    
+</div>
+    
+</div>
+    <!-- row -->
+
+
+    </div>
 <?php include("footer.php");?>
