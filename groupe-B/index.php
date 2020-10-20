a8c191b61b702fc5e862d5f80617504d35df8912
Petra Bezpaloff <Petra.bezpaloff@gmail.com>
2020-10-20 13:19:02 +0200

    cards essais

--- groupe-B/index.php
+++ groupe-B/index.php
@@ -1,5 +1,40 @@
 <?php include('header.php');?>
 
-	accueil
+<div class="wrapper">
+  <h1 class="text-center">Evenements à la journée Ferrer</h1>
+  <hr />
+  <div class="row">
+    <div class="col-md-4">
+      <div class="card">
+    <img src="https://placeimg.com/640/480/arch" />
+    <div class="card-body">
+      <h3>Nature writing</h3>
+    <p class="text-justify">Nature writing often draws heavily on scientific information and facts about the natural world; at the same time, it is frequently written in the first person and incorporates personal observations of and philosophical reflections upon nature.</p>
+    <button type="button" class="btn btn-primary w-25">View</button>
+    </div>
+  </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+    <img src="https://placeimg.com/640/480/arch" />
+    <div class="card-body">
+      <h3>Nature writing</h3>
+    <p class="text-justify">Nature writing often draws heavily on scientific information and facts about the natural world; at the same time, it is frequently written in the first person and incorporates personal observations of and philosophical reflections upon nature.</p>
+    <button type="button" class="btn btn-primary w-50">View</button>
+    </div>
+  </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+    <img src="https://placeimg.com/640/480/arch" />
+    <div class="card-body">
+      <h3>Nature writing</h3>
+    <p class="text-justify">Nature writing often draws heavily on scientific information and facts about the natural world; at the same time, it is frequently written in the first person and incorporates personal observations of and philosophical reflections upon nature.</p>
+    <button type="button" class="btn btn-primary w-25">View</button>
+    </div>
+  </div>
+    </div>
+  </div>
+</div>
 
 <?php include('footer.php');?>
