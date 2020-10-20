709b2f16ed87af4db18e91cf463ef73d74e59239
Sulaiman El Moussati <sulakyoku@gmail.com>
2020-10-20 15:49:16 +0200

    dernier mod avant rec de julien

--- groupe-A/detail.php
+++ groupe-A/detail.php
@@ -7,10 +7,19 @@
         <div class="row">
             <div class="col-12 col-md-6 imgevent">
                 <h4 class="catevent">Economie</h4>
-                img
+                <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
             </div>
             <div class="col-12 col-md-6 txtevent">
-            txt
+            <h1>Atelier Ecape Game</h1>
+                        <h4>De 11h à 13h</h4>
+                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
+                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
+
+                        <span>Aneessens</span>
+                        <h5>local 140</h5>
+
+                        
+                        <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
             </div>                               
         </div>
     </div>
