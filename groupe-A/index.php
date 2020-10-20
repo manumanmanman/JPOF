0e84835d66c7e2295682596a0722f63cf2d49702
Julien De coen <juliendecoen2308@gmail.com>
2020-10-20 13:50:16 +0200

    style cartes ok

--- groupe-A/index.php
+++ groupe-A/index.php
@@ -1,60 +1,60 @@
+
 <?php require "header.php" ?>
-<?php echo "Julien a ajouté quelque chose" ?>
-<?php echo "Sulaiman a ajouté autre chose" ?>
-<?php echo "Gabriel a ajouté autre chose" ?>
+<div id="screensize"></div>
+<br>
 
 
 
 <div class="container">
         <div class="row">
 
-            <div class="col-md-6 card-entier">
-                <div class="cartes">
-                    <div class="row">
-                    <div class="carte card-left col-6">
-                        <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
-                        <h2>Techniques graphiques</h2>
-                    </div>
-                    <div class="col-6 carte card-right">
-                        <h1>Atelier Ecape Game</h1>
-                        <h4>De 11h à 13h</h4>
-                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
-                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
-
-                        <span>Aneessens</span>
-                        <h5>local 140</h5>
-
-                        
-                        <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
-                    </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-6 card-entier">
-                <div class="cartes">
-                    <div class="row">
-                    <div class="carte card-left col-6">
-                        <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
-                        <h2>Techniques graphiques</h2>
-                    </div>
-                    <div class="col-6 carte card-right">
-                        <h1>Atelier Ecape Game</h1>
-                        <h4>De 11h à 13h</h4>
-                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
-                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
-
-                        <span>Aneessens</span>
-                        <h5>local 140</h5>
-
-                        
-                        <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
-                    </div>
-                    </div>
-                </div>
+            <div class="col-lg-6 card-entier">
                 
-            </div>
+                    <div class="cartes row">
+                    <div class="carte card-left col-6">
+                        <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
+                        <h2>Techniques graphiques</h2>
+                    </div>
+                    <div class="col-6 carte card-right">
+                        <h1>Atelier Ecape Game</h1>
+                        <h4>De 11h à 13h</h4>
+                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
+                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
 
-        </div>
-    </div>
+                        <span>Aneessens</span>
+                        <h5>local 140</h5>
+
+                        
+                        <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
+                    </div>
+                    </div>
+                
+            </div> <!--  card fin -->
+
+            <div class="col-lg-6 card-entier">
+              
+                    <div class="cartes row">
+                    <div class="carte card-left col-6">
+                        <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
+                        <h2>Techniques graphiques</h2>
+                    </div>
+                    <div class="col-6 carte card-right">
+                        <h1>Atelier Ecape Game</h1>
+                        <h4>De 11h à 13h</h4>
+                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
+                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
+
+                        <span>Aneessens</span>
+                        <h5>local 140</h5>
+
+                        
+                        <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
+                    </div>
+                    </div>
+                
+                
+            </div> <!-- card fin -->
+
+        </div> <!-- row fin -->
+    </div> <!-- container fin -->
 <?php require "footer.php" ?>
