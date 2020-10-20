980e367f8a66ccf3312029f65089b67dffd2827b
Eden Maestro <edenkonde@gmail.com>
2020-10-20 13:09:34 +0200

    Rajout PlaceHolder pour le header

--- groupe-B/header.php
+++ groupe-B/header.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>page-web-type</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-	<link href="scss/style.css" rel="stylesheet" type="text/css">
+	<link href="scss/_eden.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-white">
--- groupe-B/index.php
+++ groupe-B/index.php
@@ -1,5 +1,15 @@
 <?php include('header.php');?>
 
-	accueil
+	<div class="">
+		<div class="">
+		<img class="d-block w-100" src="https://via.placeholder.com/1290x400/" alt="First slide">
+		</div>
+	</div>
+
+
+
+
+
+
 
 <?php include('footer.php');?>
