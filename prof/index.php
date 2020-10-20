a9993a5bf9ba12a6b01d03525e6862bcbeb4ba48
Emmanuel Lemal <man@elastik.eu>
2020-10-20 13:09:13 +0200

    ajouté fichiers Julien

---
+++ prof/index.php
@@ -0,0 +1,115 @@
+<?php require "header.php" ?>
+<div class="container">
+        <div class="row">
+
+            <div class="col-lg-6 card-entier">
+              
+                    <div class="cartes row">
+                    <div class="carte card-left col-6">
+                        <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
+                        <h2>Techniques graphiques</h2>
+                    </div>
+                    <div class="col-6 carte card-right">
+                        <h1>Atelier Ecape Game</h1>
+                        <h4>De 11h à 13h</h4>
+                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
+                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
+
+                        <span>Aneessens</span>
+                        <h5>local 140</h5>
+
+                        
+                        <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
+                    </div>
+                    </div>
+             
+            </div> <!-- card entier -->
+
+
+            <div class="col-lg-6 card-entier">
+           
+                    <div class="cartes row">
+                    <div class="carte card-left col-6">
+                        <img src="img/vue.jpeg" alt="escape-room">
+                        <h2>Techniques graphiques</h2>
+                    </div>
+                    <div class="col-6 carte card-right">
+                        <h1>Atelier Ecape Game</h1>
+                        <h4>De 11h à 13h</h4>
+                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
+                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
+
+                        <span>Aneessens</span>
+                        <h5>local 140</h5>
+
+                        
+                        <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
+                    </div>
+                
+                </div>
+                
+            </div>  <!-- card entier -->
+
+            <div class="col-lg-6 card-entier">
+           
+                    <div class="cartes row">
+                    <div class="carte card-left col-6">
+                        <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
+                        <h2>Techniques graphiques</h2>
+                    </div>
+                    <div class="col-6 carte card-right">
+                        <h1>Atelier Ecape Game</h1>
+                        <h4>De 11h à 13h</h4>
+                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
+                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
+
+                        <span>Aneessens</span>
+                        <h5>local 140</h5>
+
+                        
+                        <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
+                    </div>
+                
+                </div>
+                
+            </div>  <!-- card entier -->
+
+            <div class="col-lg-6 card-entier">
+           
+                    <div class="cartes row">
+                    <div class="carte card-left col-6">
+                        <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
+                        <h2>Techniques graphiques</h2>
+                    </div>
+                    <div class="col-6 carte card-right">
+                        <h1>Atelier Ecape Game</h1>
+                        <h4>De 11h à 13h</h4>
+                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
+                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
+
+                        <span>Aneessens</span>
+                        <h5>local 140</h5>
+
+                        
+                        <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
+                    </div>
+                
+                </div>
+                
+            </div>  <!-- card entier -->
+
+        </div>
+    </div>
+
+
+<?php
+
+
+?>
+
+ <!-- LEFT JOIN locaux ON activities.room_id = rooms.room_id
+                  LEFT JOIN buildings ON activities.building_id = buildings.building_id
+                   LEFT JOIN speakers ON activities.activity_speaker = speakers.speakers_id -->
+
+
+<?php require "footer.php" ?>
