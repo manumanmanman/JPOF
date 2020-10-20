2a6713fc28aaadfe10044a44837b7c6a4ed0a8f4
Camille Scalais <camille_scalais@hotmail.com>
2020-10-20 16:51:22 +0200

    footer fait

--- groupe-B/footer.php
+++ groupe-B/footer.php
@@ -1,5 +1,29 @@
 <script src="js/jquery-3.4.1.min.js"></script>	
 <script src="js/bootstrap.min.js"></script>
 <script src="js/custom.js"></script>
+
+
+<footer id="navbar-footer">
+
+<ul class="nav flex-column">
+    <li class="nav-item">
+        <a class="nav-link active nav-link-footer" href="index.php">Accueil</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link nav-link-footer" href="activites.php">Evénements</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link nav-link-footer" href="favoris.php">Favoris</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link nav-link-footer" href="conferenciers.php">Profil</a>
+    </li>
+</ul>
+
+<div class="heff-footer">Haute Ecole Francisco Ferrer</div>
+</footer>
+
+
+
 </body>
 </html>
