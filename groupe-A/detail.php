9a6609e010dfcb2741d4409c5687a4f6d68fc4c5
Sulaiman El Moussati <sulakyoku@gmail.com>
2020-10-20 09:48:18 +0200

    modification2

--- groupe-A/detail.php
+++ groupe-A/detail.php
@@ -1,3 +1,20 @@
 <?php require "header.php" ?>
 
+
+
+<div class="container">
+    <div class="event">
+        <div class="row">
+            <div class="col-12 col-md-6 imgevent">
+                <h4 class="catevent">Economie</h4>
+                img
+            </div>
+            <div class="col-12 col-md-6 txtevent">
+            txt
+            </div>                               
+        </div>
+    </div>
+</div>
+    
+
 <?php require "footer.php" ?>
