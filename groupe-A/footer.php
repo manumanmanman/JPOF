0afd4ddcba57ff10f38d03d5172b34ce36c6f525
Julien De coen <juliendecoen2308@gmail.com>
2020-10-26 11:44:00 +0100

    homepage 90%

--- groupe-A/footer.php
+++ groupe-A/footer.php
@@ -1,3 +1,54 @@
+<div class="container-fluid footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-3">
+                    <h2>COORDONNÉES</h2>
+
+                    <p>Siège social
+                    rue de la Fontaine 4
+                    1000 BRUXELLES<p>
+
+                    <p>Tél : +32 2 279 58 10<p>
+                    <p>Fax : +32 2 279 58 29<p>
+
+                    <a href="#" target= "_blank">Nos implantations</a>
+
+                </div>
+
+
+                <div class="col-3">
+                    <h2>PRATIQUE</h2>
+
+                    <a href="#" target= "_blank">Contact</a><br>
+                    <a href="#" target= "_blank">Partenaires</a><br>
+                    <a href="#" target= "_blank">Offres d'emploi</a><br>
+                    <a href="#" target= "_blank">Newsletter</a><br>
+                    <a href="#" target= "_blank">Mentions légales</a><br>
+                    <a href="#" target= "_blank">Plan du site</a><br>
+                    <a href="#" target= "_blank">Accessibilité</a><br>
+                </div>
+
+
+                <div class="col-3">
+                    <a href="#" target= "_blank">Facebook</a><br>
+                    <a href="#" target= "_blank">Twitter </a><br>
+                    <a href="#" target= "_blank">LinkedIn </a><br>
+                    <a href="#" target= "_blank">Flickr </a><br>
+                    <a href="#" target= "_blank">RSS</a><br>
+                </div>
+
+
+                <div class="col-3">
+                    <img src="img/bxl_logo.png" alt="logo ville de Bruxelles">
+                </div>
+
+ 
+                
+            </div> <!-- row fin -->
+        </div> <!-- container fin -->
+</div> <!-- container-fluid fin -->
+    
     <script src="js/jquery-3.5.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/index.js"></script>
--- groupe-A/index.php
+++ groupe-A/index.php
@@ -1,12 +1,11 @@
-
 <?php require "header.php" ?>
 <div id="screensize"></div>
 <br>
 
 
-
-<div class="container">
+<div class="container divactivite1">
         <div class="row">
+            <h1 class="col-12 activite">Activitées de la journée Ferrer 2021</h1>
 
             <div class="col-lg-6 card-entier pedagogique">
                 
@@ -80,6 +79,159 @@
                 
             </div> <!-- card fin -->
 
+            <div class="col-lg-6 card-entier pedagogique">
+                
+                    <div class="cartes row">
+                    <div class="carte card-left col-6">
+                        <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
+                        <h2>pedagogique</h2>
+                    </div>
+                    <div class="col-6 carte card-right">
+                        <h1>Atelier Ecape Game</h1>
+                        <h4>De 11h à 13h</h4>
+                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
+                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
+
+                        <span>Aneessens</span>
+                        <h5>local 140</h5>
+
+                        
+                        <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
+                    </div>
+                    </div>
+                
+            </div> <!--  card fin -->
+
         </div> <!-- row fin -->
     </div> <!-- container fin -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="container-fluid divactivite2">
+        <div class="container">
+        <div class="row">
+            <h1 class="col-12 activite">Activitées de la journée Porte ouverte 2021</h1>
+
+            <div class="col-lg-6 card-entier pedagogique">
+                
+                    <div class="cartes row">
+                    <div class="carte card-left col-6">
+                        <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
+                        <h2>pedagogique</h2>
+                    </div>
+                    <div class="col-6 carte card-right">
+                        <h1>Atelier Ecape Game</h1>
+                        <h4>De 11h à 13h</h4>
+                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
+                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
+
+                        <span>Aneessens</span>
+                        <h5>local 140</h5>
+
+                        
+                        <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
+                    </div>
+                    </div>
+                
+            </div> <!--  card fin -->
+
+            <div class="col-lg-6 card-entier paramedical">
+              
+                    <div class="cartes row">
+                    <div class="carte card-left col-6">
+                        <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
+                        <h2>Paramedical</h2>
+                    </div>
+                    <div class="col-6 carte card-right">
+                        <h1>Atelier Ecape Game</h1>
+                        <h4>De 11h à 13h</h4>
+                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
+                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
+
+                        <span>Aneessens</span>
+                        <h5>local 140</h5>
+
+                        
+                        <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
+                    </div>
+                    </div>
+                
+                
+            </div> <!-- card fin -->
+
+
+            <div class="col-lg-6 card-entier">
+              
+                    <div class="cartes row">
+                    <div class="carte card-left col-6">
+                        <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
+                        <h2>Techniques graphiques</h2>
+                    </div>
+                    <div class="col-6 carte card-right">
+                        <h1>Atelier Ecape Game</h1>
+                        <h4>De 11h à 13h</h4>
+                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
+                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
+
+                        <span>Aneessens</span>
+                        <h5>local 140</h5>
+
+                        
+                        <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
+                    </div>
+                    </div>
+                
+                
+            </div> <!-- card fin -->
+
+            <div class="col-lg-6 card-entier pedagogique">
+                
+                    <div class="cartes row">
+                    <div class="carte card-left col-6">
+                        <img src="img/escape-room-escape-game-orleans.jpg" alt="escape-room">
+                        <h2>pedagogique</h2>
+                    </div>
+                    <div class="col-6 carte card-right">
+                        <h1>Atelier Ecape Game</h1>
+                        <h4>De 11h à 13h</h4>
+                        <a class= "coeur" href=""><img src="img/image-coeur-png-blanc.png" alt="coeur"></a>
+                        <p>Dans cette atelier, vous allez dévelloper vos sens et votre logique en vous laissant dans des enigmes ...</p>
+
+                        <span>Aneessens</span>
+                        <h5>local 140</h5>
+
+                        
+                        <a href="#" class= "btn btn-success inscription">Je m'inscris</a>
+                    </div>
+                    </div>
+                
+            </div> <!--  card fin -->
+            </div><!-- container fin -->
+        </div> <!-- row fin -->
+    </div> <!-- container-fluid fin -->
+
+
+
+
+
+
+
+
+
+
+
+
 <?php require "footer.php" ?>
