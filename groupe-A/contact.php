5b15a3cbd007bb302801b0a041315e8d1b8da27b
Sulaiman El Moussati <sulakyoku@gmail.com>
2020-10-27 01:22:54 +0100

    debut page contact

--- groupe-A/contact.php
+++ groupe-A/contact.php
@@ -1,4 +1,33 @@
 <?php require "header.php" ?>
 
+<div id="screensize"></div>
+<br>
+
+
+<div class="container">
+    <div class="event">
+        <div class="row">
+            <div class="col-12 col-md-4 google-maps">
+                
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.436087895105!2d4.3417609152810925!3d50.84160836698417!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c470f8e9e6e5%3A0xce8f6e6d0dc0ab14!2sHaute%20%C3%89cole%20Francisco%20Ferrer!5e0!3m2!1sfr!2sbe!4v1603753975273!5m2!1sfr!2sbe"  frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
+            <div class="col-12 col-md-8 contact">
+                <h1>Contact</h1>
+                        <div>
+                        <h4>Vous pouvez nous contacter par mail : <a href="#">JPOF@mail.com</a></h4>
+                        <h4>Vous pouvez nous contacter par téléphone : <a href="#">0465235689</a></h4>
+                        <h4>Notre adresse disponble pour courriel : <a href="#">4 rue de midi, 1000 Bruxelles</a></h4>
+                        </div>
+                      
+
+                        
+                      
+            </div>                               
+        </div>
+    </div>
+</div>
+    
+
+
 <?php require "footer.php" ?>
 
