460fc00f12b6d179602d1d258e70205fc890affd
Sarah Haddou <sarah.haddou@outlook.com>
2020-11-11 00:37:47 +0100

    sans api

--- groupe-D/about.php
+++ groupe-D/about.php
@@ -96,10 +96,8 @@ Cette année, ce n'est pas moins de 50 activités qui prendront place sur 4 impl
 
 
 <div class="col-12 col-lg-6 rcampus">
-
-<div id="mappy"></div>
-<div id="contenu"></div>
-
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.4903577753203!2d4.340286716032571!3d50.84060327953117!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c470f84554f5%3A0x94c79c309d9e1e21!2sRue%20de%20la%20Fontaine%204%2C%201000%20Bruxelles!5e0!3m2!1sfr!2sbe!4v1603795659574!5m2!1sfr!2sbe" 
+        width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
  </div>
  
 
