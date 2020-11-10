46dedddaa07bb091e73df2e3e878b0a76d4c7e3a
Gabriel T <gabriel.torres.silva@hotmail.com>
2020-11-10 10:40:35 +0100

    Dynamisation conférenciers

--- groupe-A/conferencier.php
+++ groupe-A/conferencier.php
@@ -1,4 +1,4 @@
-<?php require "header.php" ?>
+<?php require "header.php";require "admin/includes/conn.inc.php" ?>
 
 <div id="screensize"></div>
 
@@ -9,96 +9,32 @@
         <div class="row rowconf">
 
             <h1 class="col-12 titreconferencier">Les conférenciers</h1>
-
-            <div class="col-lg-6 conferencier">
-                    
-                    <div class="row conftout">
-
-                        <div class="conf-left col-6">
-                            <a href="detail-speaker.php"><img src="img/escape-room-escape-game-orleans.jpg" alt=""></a>
-                            
-                        </div><!--  left fin -->
-
-                        <div class="col-6 conf-right">
-                            <a href="detail-speaker.php"><h1>Jean Boffrot</h1></a>
-                            <a href="linkedin/prof=Jeanboffrot"><img src="img/linkedin.png" alt="profil linkedin de Jean Boffrot"></a><br>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum suscipit adipisci beatae nemo quia rem accusamus fugiat neque veniam quisquam assumenda fugit eius cumque.</p>
-                        </div><!--  right fin -->
+            <?php 
+            $sql = "SELECT * FROM speakers";
+            $result = $conn->query($sql);
+            foreach($result as $row){
+                echo "<div class='col-lg-6 conferencier'>
                         
-                    </div> <!--  row fin -->
-                    
-                </div> <!--  conferencier fin -->
+                        <div class='row conftout'>
+    
+                            <div class='conf-left col-6'>
+                                <a href='detail-speaker.php'><img src='".$row["speaker_pfp"]."' alt=''></a>
+                                
+                            </div><!--  left fin -->
+    
+                            <div class='col-6 conf-right'>
+                                <a href='detail-speaker.php'><h1>".$row["speaker_name"].", ".$row["speaker_surname"]."</h1></a>
+                                <a href='linkedin/prof=Jeanboffrot'><img target='_blank' src='img/linkedin.png' alt='profil linkedin de ".$row["speaker_name"].", ".$row["speaker_surname"]."'></a><br>
 
-
-
-
-
-                <div class="col-lg-6 conferencier">
-                    
-                    <div class="row conftout">
-
-                        <div class="conf-left col-6">
-                            <a href="detail-speaker.php"><img src="img/escape-room-escape-game-orleans.jpg" alt=""></a>
+                            </div><!--  right fin -->
                             
-                        </div><!--  left fin -->
-
-                        <div class="col-6 conf-right">
-                            <a href="detail-speaker.php"><h1>Jean Boffrot</h1></a>
-                            <a href="linkedin/prof=Jeanboffrot"><img src="img/linkedin.png" alt="profil linkedin de Jean Boffrot"></a><br>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum suscipit adipisci beatae nemo quia rem accusamus fugiat neque veniam quisquam assumenda fugit eius cumque.</p>
-                        </div><!--  right fin -->
-
-                    </div> <!--  row fin -->
-                    
-                </div> <!--  conferencier fin -->
-
-
-                <div class="col-lg-6 conferencier">
-                    
-                    <div class="row conftout">
-
-                        <div class="conf-left col-6">
-                            <a href="detail-speaker.php"><img src="img/escape-room-escape-game-orleans.jpg" alt=""></a>
-                            
-                        </div><!--  left fin -->
-
-                        <div class="col-6 conf-right">
-                            <a href="detail-speaker.php"><h1>Jean Boffrot</h1></a>
-                            <a href="linkedin/prof=Jeanboffrot"><img src="img/linkedin.png" alt="profil linkedin de Jean Boffrot"></a><br>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum suscipit adipisci beatae nemo quia rem accusamus fugiat neque veniam quisquam assumenda fugit eius cumque.</p>
-                        </div><!--  right fin -->
+                        </div> <!--  row fin -->
                         
-                    </div> <!--  row fin -->
-                    
-                </div> <!--  conferencier fin -->
-
-
-
-
-                <div class="col-lg-6 conferencier">
-                    
-                    <div class="row conftout">
-
-                        <div class="conf-left col-6">
-                            <a href="detail-speaker.php"><img src="img/escape-room-escape-game-orleans.jpg" alt=""></a>
-                            
-                        </div><!--  left fin -->
-
-                        <div class="col-6 conf-right">
-                            <a href="detail-speaker.php"><h1>Jean Boffrot</h1></a>
-                            <a href="linkedin/prof=Jeanboffrot"><img src="img/linkedin.png" class="linke-lien" alt="profil linkedin de Jean Boffrot"></a><br>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum suscipit adipisci beatae nemo quia rem accusamus fugiat neque veniam quisquam assumenda fugit eius cumque.</p>
-                        </div><!--  right fin -->
-                        
-                    </div> <!--  row fin -->
-                    
-                </div> <!--  conferencier fin -->
-
-                
-            
-
+                    </div> <!--  conferencier fin -->";
+            }
+            ?>
             <div class="col-12 lienall">
-            <a href="all-activities.php" class= "btn btn-success acti">Voir toutes les activitées</a>
+            <a href="all-activities.php" class= "btn btn-success acti">Voir toutes les activités</a>
             </div>
 
         </div> <!-- row fin -->
