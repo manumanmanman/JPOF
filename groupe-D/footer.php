6447e7e1224df8e158134dbdf9455a5e3eeeb27c
Nicolas-DR <images203@hotmail.com>
2020-11-10 09:04:46 +0100

    footer nico

--- groupe-D/footer.php
+++ groupe-D/footer.php
@@ -1,6 +1,87 @@
+<!-- Footer -->
+
+<style>
+footer h5, footer p {text-align: center;}
+footer a {color: #ffffff;}
+footer p {font-size: larger;}
+footer i {    margin: 10px;}</style>
+
+<footer class="page-footer font-small text-white bg-dark pt-4 footer" style="">
+
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- row -->
+    <div class="row">
+      <div class="col-md-1 mt-md-0 mt-none">
+      </div>
+
+      <!-- column -->
+      <div class="col-md-4 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <h5 class="text-uppercase font-weight-bold text-uppercase">coordonées</h5>
+        <p>Rue de la fontaine 4<br>
+        1000 Bruxelles<br><br>
+        Tél : +32 2 279 58 10<br>
+        Fax : +32 2 279 58 10</p>
+
+      </div>
+      <!-- column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- column -->
+      <div class="col-md-4 mb-md-0 mb-1">
+
+        <!-- Content -->
+        
+        <p>Du lundi au vendredi<br>
+        Horaire : 10h - 18h
+        ferrer@mail.eu<br>
+        www.he-ferrer.eu</p>
+
+      </div>
+       <!-- column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+      
+      <!-- column -->
+            <div class="col-md-3 mb-md-0 mb-3 justify-content-center">
+
+        <!-- Content -->
+        <h5 class="text-uppercase font-weight-bold text-uppercase">Suivez-nous</h5>
+
+          <!-- Facebook -->
+          <p>
+          <a class="fb-ic">
+            <i class="fab fa-facebook-f fa-lg white-text"> </i>
+          </a>
+           <!--Linkedin -->
+           <a class="li-ic">
+            <i class="fab fa-linkedin-in fa-lg white-text"> </i>
+          </a>
+          <!-- Twitter -->
+          <a class="tw-ic">
+            <i class="fab fa-twitter fa-lg white-text"> </i>
+          </a>
+          </p>
+      </div>
+      <div class="col-md-1 mt-md-0 mt-none">
+      </div>
+      <!-- column -->
+
+    </div>
+    <!-- row -->
+
+  </div>
+
+</footer>
+<!-- Footer -->
+
+
+
 <!-- dans le footer on charge le modal qui contient le formulaire de login et enregistrement qui sera disponible sur toutes les pages -->
    <!-- Modal -->
-  
 <!--Modal: Login / Register Form-->
 <div class="modal fade" id="examplemodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog cascading-modal" role="document">
@@ -105,17 +186,25 @@
     <!--/.Content-->
   </div>
 </div>
-<!--Modal: Login / Register Form-->
 
+
+
+
+
+<!--Modal: Login / Register Form-->
+<!--
 <div class="text-center">
   <a href="" class="btn btn-default btn-rounded my-3" data-toggle="modal" data-target="#modalLRForm">Launch
     Modal LogIn/Register</a>
-</div>
+</div>-->
 
- 
+
+
+
  <script src="js/jquery-3.5.1.min.js"></script> 
  <script src="js/bootstrap.min.js"></script>  
  <script src="js/scripts.js"></script>
  
+
 </body>
 </html>
