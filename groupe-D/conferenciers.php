bd99eb33cf3ce3a44a5c6c09396c5c80c3499d5a
Ayoub Benlamin <ayoubbenlamin@hotmail.com>
2020-11-10 17:43:30 +0100

    ajout images campus + details

--- groupe-D/conferenciers.php
+++ groupe-D/conferenciers.php
@@ -20,11 +20,11 @@ foreach ($speakers as $speaker) {
 ?>
 
 
-<div class="col-12 col-md-3 conferencier">
-
+<div class="col-sm-6 col-md-4 col-lg-3 conferencier">
+<a  href="detail-conferencier.php?conferencierid=<?php echo $speaker['speaker_id'] ?>">
 <img src="<?php echo $speaker['speaker_pfp'] ?>" alt="<?php echo $speaker['speaker_name'] ?>">
 
-<h1><a  href="detail-conferencier.php?conferencierid=<?php echo $speaker['speaker_id'] ?>"><?php echo utf8_encode($speaker['speaker_surname'])." ".$speaker['speaker_name'] ?></a></h1>
+<h1><?php echo utf8_encode($speaker['speaker_surname'])." ".$speaker['speaker_name'] ?></a></h1>
 
 </div><!-- col -->
 
